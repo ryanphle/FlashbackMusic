470cbca25506510d548dd126ae1d0b3b49e72bbb
Ryan Le <ryanphle@gmail.com>
2018-02-06 15:57:41 -0800

    First additions to the main screen

--- FlashbackMusic/app/src/main/java/team21/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/team21/flashbackmusic/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,18 +22,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_songs);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_albums);
                     return true;
             }
             return false;
         }
     };
+
+    String[] dummySongs = {"song1", "song2", "song3", "song4", "song5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, dummySongs);
+
+        ListView listView = (ListView) findViewById(R.id.song_list);
+        listView.setAdapter(adapter);
     }
 
 }
