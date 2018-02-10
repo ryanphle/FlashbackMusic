7690ac326c1931384649641949daaf4b91e719fd
pettejn <pettnarv@gmail.com>
2018-02-10 14:35:10 -0800

    Issue number: 20. Modified flashback ui

--- FlashbackMusic/app/src/main/java/team21/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/team21/flashbackmusic/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                         setAlbumFragment();
                         break;
                     case R.id.navigation_flashback:
-                        fragment = new FlashbackFragment();
+                        setFlashbackFragment();
                         break;
                 }
                 final FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -111,6 +111,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void setSongFragment() {
         fragment = new SongsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList("songs", songs);
+        fragment.setArguments(bundle);
+    }
+
+    private void setFlashbackFragment() {
+        fragment = new FlashbackFragment();
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList("songs", songs);
         fragment.setArguments(bundle);
