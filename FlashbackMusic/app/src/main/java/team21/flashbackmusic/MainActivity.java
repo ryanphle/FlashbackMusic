b85fa28e1f0d955272ca76a473a2e26ced6d2a0a
pettejn <pettnarv@gmail.com>
2018-03-14 22:57:46 -0700

    Changed use of database tables

--- FlashbackMusic/app/src/main/java/team21/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/team21/flashbackmusic/MainActivity.java
@@ -665,12 +665,12 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
         String ID = hashFunction(name + artist);
 
-        myRef.child("TestSongs").child(ID).child("Title").setValue(name);
-        myRef.child("TestSongs").child(ID).child("Url").setValue(url);
-        myRef.child("TestSongs").child(ID).child("Artist").setValue(artist);
-        myRef.child("TestSongs").child(ID).child("Uri").setValue(uri.toString());
-        myRef.child("TestSongs").child(ID).child("Img").setValue(img.toString());
-        myRef.child("TestSongs").child(ID).child("Album").setValue(album);
+        myRef.child("Songs").child(ID).child("Title").setValue(name);
+        myRef.child("Songs").child(ID).child("Url").setValue(url);
+        myRef.child("Songs").child(ID).child("Artist").setValue(artist);
+        myRef.child("Songs").child(ID).child("Uri").setValue(uri.toString());
+        myRef.child("Songs").child(ID).child("Img").setValue(img.toString());
+        myRef.child("Songs").child(ID).child("Album").setValue(album);
     }
 
     public void initialFragSetup(int frag) {
@@ -1186,7 +1186,7 @@ public class MainActivity extends AppCompatActivity {
         });
         sorted_songs = new ArrayList<>();
         Location location_song = new Location(lastLocation);
-        for (DataSnapshot song : allPlays.child("TestSongs").getChildren()){
+        for (DataSnapshot song : allPlays.child("Songs").getChildren()){
             boolean added = false;
             String ID = song.getKey();
             for (Song existingSong : songs){
@@ -1292,13 +1292,13 @@ public class MainActivity extends AppCompatActivity {
                 if (dataSnapshot.child("Users").child(myUserID).exists()){
                     myProxyName = dataSnapshot.child("Users").child(myUserID).child("Proxy").getValue(String.class);
                 } else {
-                    Iterable<DataSnapshot> proxies = dataSnapshot.child("Proxy Test").getChildren();
+                    Iterable<DataSnapshot> proxies = dataSnapshot.child("Proxy").getChildren();
                     for (DataSnapshot proxy : proxies) {
                         //FirebaseDatabase.getInstance().getReference().child("Users").child(userID).child("Proxy").setValue(proxy.getValue(String.class));
                         myProxyName = proxy.getValue(String.class);
                         FirebaseDatabase.getInstance().getReference().child("Users").child(myUserID).child("Proxy").setValue(proxy.getValue(String.class));
                         FirebaseDatabase.getInstance().getReference().child("Users").child(myUserID).child("Username").setValue(myUserName);
-                        ref.child("Proxy Test").child(proxy.getValue(String.class)).removeValue();
+                        ref.child("proxy").child(proxy.getValue(String.class)).removeValue();
                         break;
                     }
                 }
